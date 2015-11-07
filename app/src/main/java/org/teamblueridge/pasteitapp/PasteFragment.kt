8d4c014a7cd256ac939b88af81c99362fc70e34d
Kyle Laker <10kylelaker@gmail.com>
2015-11-07 16:03:59 -0500

    PasteFragment: Transition to Kotlin

---
+++ app/src/main/java/org/teamblueridge/pasteitapp/PasteFragment.kt
@@ -0,0 +1,21 @@
+package org.teamblueridge.pasteitapp
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Loads the view for the Paste fragment
+ */
+class PasteFragment : Fragment() {
+    //private static final String TAG = "TeamBlueRidge";
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle): View? {
+        val rootView = inflater.inflate(R.layout.fragment_paste, container, false)
+        (activity as MainActivity).setActionBarTitle(getString(R.string.app_name))
+        return rootView
+    }
+}
